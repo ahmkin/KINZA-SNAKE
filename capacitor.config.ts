5b8de510d98da55d983aec4f5cbd3a4272345808
Ahmad Hussain <ahmii@Ahmads-MacBook-Air.local>
2026-09-02 17:59:42 +0500

    Fixed capacitor setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,19 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.kinza.snake',
   appName: 'KINZA SNAKE',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https',
-    cleartext: true,
-  },
-  android: {
-    backgroundColor: '#03130b',
-    allowMixedContent: true,
-    buildOptions: {
-      keystorePath: undefined,
-      keystoreAlias: undefined,
-    },
-  },
+  webDir: 'dist'
 };
 
 export default config;
